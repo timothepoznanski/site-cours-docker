293f30dff1a0d2f8cc590260041dbc4e41791469
Timothé Poznanski <tim.poznanski@gmail.com>
2024-01-11 00:13:28 +0100

    Create index.php

---
+++ mon-site/index.php
@@ -0,0 +1,17 @@
+<?php   
+  
+$servername = 'db';  
+$username = 'user';  
+$password = 'password';  
+$dbname = 'mydb';  
+  
+$conn = mysqli_connect($servername, $username, $password, $dbname);   
+    
+$sql = "SELECT nom, points FROM tableau_resultats";   
+$result = mysqli_query($conn, $sql);   
+    
+while($row = mysqli_fetch_assoc($result)) {   
+  echo "Nom : " . $row["nom"] . " - Points : " . $row["points"] . "<br>";   
+}   
+  
+?>
