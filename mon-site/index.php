6a59f32b98686006193abf3e18daa4addb5ea81c
Timothé Poznanski <tim.poznanski@gmail.com>
2024-01-11 01:38:45 +0100

    Update index.php

--- mon-site/index.php
+++ mon-site/index.php
@@ -1,17 +1,28 @@
-<?php   
-  
-$servername = 'db';  
-$username = 'user';  
-$password = 'password';  
-$dbname = 'mydb';  
-  
-$conn = mysqli_connect($servername, $username, $password, $dbname);   
-    
-$sql = "SELECT nom, points FROM tableau_resultats";   
-$result = mysqli_query($conn, $sql);   
-    
-while($row = mysqli_fetch_assoc($result)) {   
-  echo "Nom : " . $row["nom"] . " - Points : " . $row["points"] . "<br>";   
-}   
-  
+<?php
+
+$servername = 'db';
+$username = 'user';
+$password = 'password';
+$dbname = 'mydb';
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+$sql = "SELECT nom, points FROM tableau_resultats";
+$result = mysqli_query($conn, $sql);
+
+echo "<table>";
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<form action='edit.php' method='post'>";
+    echo "<td><input type='text' name='newName' value='" . $row["nom"] . "'></td>";
+    echo "<td>Points : " . $row["points"] . "</td>";
+    echo "<td><input type='hidden' name='originalName' value='" . $row["nom"] . "'></td>";
+    echo "<td><input type='submit' value='Modifier'></td>";
+    echo "</form>";
+    echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($conn);
+
 ?>
