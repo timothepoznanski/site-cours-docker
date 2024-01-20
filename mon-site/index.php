1d9b53d4cdb4a3a1e73b5fadfcf9e208d16b711d
Timothé Poznanski <tim.poznanski@gmail.com>
2024-01-21 00:14:02 +0100

    Update index.php

--- mon-site/index.php
+++ mon-site/index.php
@@ -48,31 +48,30 @@
 <body>
     <?php
     $servername = 'db';
-$username = 'user';
-$password = 'password';
-$dbname = 'mydb';
+    $username = 'user';
+    $password = 'password';
+    $dbname = 'mydb';
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-$sql = "SELECT nom, points FROM tableau_resultats";
-$result = mysqli_query($conn, $sql);
+    $sql = "SELECT nom, points FROM tableau_resultats";
+    $result = mysqli_query($conn, $sql);
 
-echo "<table>";
-while ($row = mysqli_fetch_assoc($result)) {
-    echo "<tr>";
-    echo "<form action='edit.php' method='post'>";
-    echo "<td><input type='text' name='newName' value='" . $row["nom"] . "'></td>";
-    echo "<td>Points : " . $row["points"] . "</td>";
-    echo "<td><input type='hidden' name='originalName' value='" . $row["nom"] . "'></td>";
-    echo "<td><input type='submit' value='Modifier'></td>";
-    echo "</form>";
-    echo "</tr>";
-}
-echo "</table>";
+    echo "<table>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>";
+        echo "<form action='edit.php' method='post'>";
+        echo "<td>" . $row["nom"] . "</td>"; // Colonne pour le nom actuel
+        echo "<td><input type='text' name='newName' value=''></td>"; // Colonne pour le nouveau nom
+        echo "<td>Points : " . $row["points"] . "</td>";
+        echo "<td><input type='hidden' name='originalName' value='" . $row["nom"] . "'></td>";
+        echo "<td><input type='submit' value='Modifier'></td>";
+        echo "</form>";
+        echo "</tr>";
+    }
+    echo "</table>";
 
-mysqli_close($conn);
+    mysqli_close($conn);
     ?>
 </body>
 </html>
-
-
